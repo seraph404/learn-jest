e4374e107bf41e50b1460ae8c78e73b9d29fc3ce
Seraphina <211722638+seraph404@users.noreply.github.com>
2025-07-21 15:48:53 -0400

    Create caesarCipher function to pass first test

--- functions.js
+++ functions.js
@@ -19,7 +19,51 @@ const calculator = {
   },
 };
 
+function caesarCipher(string) {
+  const ALPHABET = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  SHIFT = 3;
+  const stringArr = string.split("");
+  const newArr = [];
+  stringArr.forEach((letter) => {
+    // find the index of the letter in the alphabet
+    const index = ALPHABET.indexOf(letter);
+    // increment the index by SHIFT
+    const newIndex = index + SHIFT;
+    newArr.push(ALPHABET[newIndex]);
+  });
+  return newArr.join("");
+}
+
+caesarCipher("abc");
 module.exports = {
   capitalize,
   calculator,
+  caesarCipher,
 };
