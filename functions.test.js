ab8a0f000df7558e61cba4bde83a4b0915e0052d
Seraphina <211722638+seraph404@users.noreply.github.com>
2025-07-21 14:00:33 -0400

    Create tests for calculator object

--- functions.test.js
+++ functions.test.js
@@ -1,9 +1,21 @@
-// test('adds 1 + 2 to equal 3', () => {
-//   expect(sum(1, 2)).toBe(3);
-// });
-
 const capitalize = require("./functions");
 
 test("capitalizes the first character of a string", () => {
   expect(capitalize("cat")).toBe("Cat");
+});
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(calculator.sum(1, 2)).toBe(3);
+});
+
+test("subtracts 2 - 1 to equal 1", () => {
+  expect(calculator.difference(2, 1)).toBe(1);
+});
+
+test("multiplies 5 * 2 to equal 10", () => {
+  expect(calculator.product(5, 2)).toBe(10);
+});
+
+test("divides 10 by 5 to equal 2", () => {
+  expect(calculator.quotient(10, 5)).toBe(2);
 });
